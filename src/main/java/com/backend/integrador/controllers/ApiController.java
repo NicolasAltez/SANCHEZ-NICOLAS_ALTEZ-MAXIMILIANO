3dd4d0489977e217bead417ee599245f940696c7
Nicolas Altez <nicolasaltezacosta27@gmail.com>
2024-03-05 19:45:18 -0300

    se agrega dependencia starter-web

--- src/main/java/com/backend/integrador/controllers/ApiController.java
+++ src/main/java/com/backend/integrador/controllers/ApiController.java
@@ -1,12 +1,15 @@
 package com.backend.integrador.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ApiController {
+
     @GetMapping("/ping")
-    public String getPing(){
-        return "api is working!";
+    public String api() {
+        return "API is working";
     }
 }
