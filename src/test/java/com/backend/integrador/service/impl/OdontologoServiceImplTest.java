e0d5bb2cde6f1e197f268715b4b2622cf8b1004c
Nicolas Altez <nicolasaltezacosta27@gmail.com>
2024-03-05 02:11:04 -0300

    se modifica conexion a base de datos y se agregan tests

--- src/test/java/com/backend/integrador/service/impl/OdontologoServiceImplTest.java
+++ src/test/java/com/backend/integrador/service/impl/OdontologoServiceImplTest.java
@@ -4,21 +4,34 @@ import com.backend.integrador.dao.impl.OdontologoDaoH2;
 import com.backend.integrador.entity.Odontologo;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+
 public class OdontologoServiceImplTest {
 
     private OdontologoServiceImpl odontologoService;
 
     @Test
     public void cuandoSeGuardaSatisfactoriamenteUnOdontologoDeberiaDevolverUnOdontologoConId(){
-
         odontologoService = new OdontologoServiceImpl(new OdontologoDaoH2());
-
         Odontologo odontologo = new Odontologo("AE2132132", "Nicolas","Sanchez");
-
         Odontologo odontologoGuardado = odontologoService.guardarOdontologo(odontologo);
-        assertNotNull(odontologoGuardado);
-        assertEquals(1, odontologo.getId());
+        assertNotNull(odontologoGuardado.getId());
     }
 
+    @Test
+    public void cuandoSeGuardaUnOdontologoConDatosNulosDeberiaDevolverUnOdontologoConIdNulo(){
+        odontologoService = new OdontologoServiceImpl(new OdontologoDaoH2());
+        Odontologo odontologo = new Odontologo(null, null,null);
+        Odontologo odontologoGuardado = odontologoService.guardarOdontologo(odontologo);
+        assertNull(odontologoGuardado);
+    }
+    @Test
+    public void cuandoSeBuscanTodosLosOdontologosDeberiaDevolverUnaListaConOdontologos(){
+        odontologoService = new OdontologoServiceImpl(new OdontologoDaoH2());
+        List<Odontologo> odontologoList = odontologoService.buscarTodosLosOdontologos();
+        assertFalse(odontologoList.isEmpty());
+    }
 }
