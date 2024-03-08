693b52648e217b5fdf5a351ba29b01e8e618e19b
Nicolas Altez <nicolasaltezacosta27@gmail.com>
2024-03-07 22:40:05 -0300

    CLINICA-PACIENTE: SE Modifican espacios

--- src/main/java/com/backend/integrador/entity/Domicilio.java
+++ src/main/java/com/backend/integrador/entity/Domicilio.java
@@ -4,11 +4,8 @@ public class Domicilio {
 
     private int id;
     private String calle;
-
     private int numero;
-
     private String localidad;
-
     private String provincia;
 
     public Domicilio(int id, String calle, int numero, String localidad, String provincia) {
