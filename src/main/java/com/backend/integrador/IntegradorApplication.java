b2ea153431e470412062a707f6c13ae3c1f87aa2
Nicolas Altez <nicolasaltezacosta27@gmail.com>
2024-03-12 10:59:31 -0300

    se implementan metodos de paciente

--- src/main/java/com/backend/integrador/IntegradorApplication.java
+++ src/main/java/com/backend/integrador/IntegradorApplication.java
@@ -1,9 +1,11 @@
 package com.backend.integrador;
 
+import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -17,4 +19,6 @@ public class IntegradorApplication {
         SpringApplication.run(IntegradorApplication.class, args);
         LOGGER.info("Aplicación corriendo");
     }
+
+
 }
